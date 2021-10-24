bdbc237ec23dd1da7f1bf59313b4097018512d49
Marcelo Ian <marceloian5@gmail.com>
2021-10-24 16:44:57 -0300

    add esquema da main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/************************************************************
+*   EDSA - EFFICIENT DISTRIBUTION SYSTEM ALGORITHM
+*       Igor Correa Rodrigues       - UFJF
+*       João Stephan Silva Maurício - UFJF
+*       Marcelo Ian Rezende Menezes - UFJF
+************************************************************/
+
+#include <iostream>
+
+#include "Grafo.hpp"
+
+int main(int argc, const char *const argv[])
+{
+    // TODO
+    return 0;
+}

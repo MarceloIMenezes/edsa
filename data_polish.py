2552978ac2fd1720fddcd82e4184dad5ce3126be
Joao Stephan <joaossmauricio@gmail.com>
2021-10-26 16:06:50 -0300

    feat: limpeza dos dados verificando a existencia de depositos iguais

--- data_polish.py
+++ data_polish.py
@@ -19,7 +19,7 @@ for i in range(0, df.shape[0] - 1):
     if df['ID'][i] == df['ID'][i+1]:
         print('O próximo é igual nesse índice:' + str(i))
 
-# Agrupa valores iguais de ID
+# # Agrupa valores iguais de ID
 # agg_func = {
 #             'Supply Site Code': 'first', 
 #             'SKU': 'first', 
@@ -37,36 +37,34 @@ for i in range(0, df.shape[0] - 1):
 
 
 # Limpeza comentada pois só será utilizada após merge de iguais
-###########################################################################
-# supply_sites = []
-# supply_sites = np.array(supply_sites)
+supply_sites = []
+supply_sites = np.array(supply_sites)
 
-# contaCenarios0 = 0
-# contaMinMaxDOC = 0
-# contaMaxDOC0 = 0
+contaCenarios0 = 0
+contaMinMaxDOC = 0
+contaMaxDOC0 = 0
 
-# # Limpeza dos dados
-# for i in range(0, len(df)):
-#     if not np.isin(df['Supply Site Code'][i], supply_sites):
-#         supply_sites = np.append(df['Supply Site Code'][i], supply_sites)  
+# Limpeza dos dados
+for i in range(0, len(df)):
+    if not np.isin(df['Supply Site Code'][i], supply_sites):
+        supply_sites = np.append(df['Supply Site Code'][i], supply_sites)  
         
-#     if df['MinDOC (Hl)'][i] > df['MaxDOC (Hl)'][i]:
-#         contaMinMaxDOC = contaMinMaxDOC + 1
-#         df.drop([i], inplace=True)    
-#     elif df['MaxDOC (Hl)'][i] == 0:
-#         contaMaxDOC0 = contaMaxDOC0 + 1
-#         df.drop([i], inplace=True)    
-#     elif df['Scenario'][i] == 0:
-#         contaCenarios0 = contaCenarios0 + 1
-#         df.drop([i], inplace=True)        
+    if df['MinDOC (Hl)'][i] > df['MaxDOC (Hl)'][i]:
+        contaMinMaxDOC = contaMinMaxDOC + 1
+        df.drop([i], inplace=True)    
+    elif df['MaxDOC (Hl)'][i] == 0:
+        contaMaxDOC0 = contaMaxDOC0 + 1
+        df.drop([i], inplace=True)    
+    elif df['Scenario'][i] == 0:
+        contaCenarios0 = contaCenarios0 + 1
+        df.drop([i], inplace=True)        
 
-# print('Cenários 0 eliminados: ' + str(contaCenarios0))
-# print('MinMaxDoc  eliminados: ' + str(contaMinMaxDOC))
-# print('MaxDoc 0   eliminados: ' + str(contaMaxDOC0))
+print('Cenários 0 eliminados: ' + str(contaCenarios0))
+print('MinMaxDoc  eliminados: ' + str(contaMinMaxDOC))
+print('MaxDoc 0   eliminados: ' + str(contaMaxDOC0))
 
-# print(supply_sites)
-###########################################################################
+print(supply_sites)
 
-# df.to_csv('clearData.csv', index=False)
+df.to_csv('clearData.csv', index=False)
 df.to_excel('clearData.xlsx', index=False)
 
